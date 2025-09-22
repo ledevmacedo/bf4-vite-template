c647108a0115152dd9c2d1a21ea5d14e37b4c083
Leandro Macedo <leandro.smacedo@Live.com>
2025-09-22 15:02:26 +0100

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
-    
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-purple-700">
+      <h1 className="text-8xl font-extrabold text-white">
+        BYTES<span className="text-teal-500">4</span>FUTURE
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
